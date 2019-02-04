98aaa77b7c33d00fe233e5143edd9642e0204590
Ben Brunton <benbrunton@hotmail.com>
2019-02-04 22:58:16 +0000

    got grabbing players

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+extern crate cfg_if;
+extern crate wasm_bindgen;
+extern crate pusoy_dos2;
+
+mod utils;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+use pusoy_dos2::game::Game;
+use pusoy_dos2::cards::Suit;
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+pub fn create_game(players: Box<[JsValue]>) -> Game {
+    utils::set_panic_hook();
+    let suit_order = [
+        Suit::Clubs,
+        Suit::Hearts,
+        Suit::Diamonds,
+        Suit::Spades
+    ];
+
+    let mut ids = vec!();
+    for player in players.iter() {
+        ids.push(JsValue::into_serde(player).unwrap());
+    }
+
+    let decks = 1;
+    let jokers = 0;
+    let reversals = false;
+
+    Game::new(decks, jokers, &ids, suit_order, reversals)
+}
+
+#[wasm_bindgen]
+pub fn get_player(game: &Game, id: &str) -> JsValue {
+    let player = game.get_player(id);
+    match player {
+        Some(p) => JsValue::from_serde(&p.get_hand()).unwrap(),
+        None    => JsValue::NULL
+    }
+}
+
+
